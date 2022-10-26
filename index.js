875d151cac131fa38e30d927b685e192adbb49c1
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-10-26 23:12:12 +0600

    fakedata nodemon error prb solved and data send

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const onlineCourses = require("./data/fakedata.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('server is running');
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+
+app.get("/courses", (req, res) => {
+    res.send(onlineCourses);
 });
 
 app.listen(port, () => {
